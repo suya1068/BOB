036555c0136979e30549d15bdbdaa6b9f6416bc4
jinseok.choi <metocherry@gmail.com>
2017-04-25 17:54:47 +0900

    충동 제거

--- src/isDate.js
+++ src/isDate.js
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-=======
 /**
  * 입력된 데이터가 데이트 타입인지 판단한다.
  * @param {*} data
  * @return {boolean}
  */
->>>>>>> 1d7766edf1e0ab478ef293f4a276b514e34d0c59
 function isDate(data) {
 	return data instanceof Date;
 }
-
